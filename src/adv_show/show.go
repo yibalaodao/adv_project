43798f304c839511ec590426d0b2304499a1cf2f
xlc <tongchao@winmeitimes.com>
2019-03-31 15:00:44 +0800

    测试连接pgsql

--- src/adv_show/show.go
+++ src/adv_show/show.go
@@ -1,6 +1,7 @@
 package adv_show
 
 import (
+	"database/sql"
 	_ "database/sql"
 	"fmt"
 	_ "github/lib/pg"
@@ -11,4 +12,20 @@ type AdvInfo struct {
 	Url     string
 	Image   string
 	DitchId int
+}
+//var db *sql.DB
+
+func sqlOpen() {
+	var err error
+	db, err = sql.Open("postgres", "port=5432 user=postgres password=laodaodao dbname=ficow sslmode=disable")
+	if err ==nil {
+		fmt.Println("连接pgsql成功")
+	}
+	//port是数据库的端口号，默认是5432，如果改了，这里一定要自定义；
+	//user就是你数据库的登录帐号;
+	//dbname就是你在数据库里面建立的数据库的名字;
+	//sslmode就是安全验证模式;
+
+	//还可以是这种方式打开
+	//db, err := sql.Open("postgres", "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full")
 }
